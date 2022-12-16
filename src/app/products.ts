7097ad5c8eb48af306ce2a0a29d331ccc2c8ccf5
kana-kobayashi <kanako.kobayashi@ctc-g.co.jp>
2022-12-17 00:31:26 +0900

    Angularフロント作成

---
+++ src/app/products.ts
@@ -0,0 +1,38 @@
+export const products = [
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    heading1: 'Heading1',
+    heading2: 'Heading2',
+    heading3: 'Heading3',
+    headingtext1: 'サンプル1',
+    headingtext2: 'サンプル2',
+    headingtext3: 'サンプル3'
+  },
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    heading1: 'Heading1',
+    heading2: 'Heading2',
+    heading3: 'Heading3',
+    headingtext1: 'サンプル1',
+    headingtext2: 'サンプル2',
+    headingtext3: 'サンプル3'
+  },
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: 'Phone Standard',
+    price: 299,
+    description: '',
+    heading1: 'Heading1',
+    heading2: 'Heading2',
+    heading3: 'Heading3',
+    headingtext1: 'サンプル1',
+    headingtext2: 'サンプル2',
+    headingtext3: 'サンプル3'
+  }
+];
